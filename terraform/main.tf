9c652efe64c866f240e1ae70cb843fcfffe4ef04
Hoai Nam NGUYEN <namnguyen107@gmail.com>
2023-09-17 22:14:44 +0200

    wip

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,18 @@ provider "aws" {
   region = "us-east-2"
 }
 
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket = "nam-state"
+    key    = "workspaces-example/terraform.tfstate"
+    region = "us-east-2"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "nam-locks"
+    encrypt        = true
+  }
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
